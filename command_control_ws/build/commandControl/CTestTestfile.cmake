7e7fcdde2f4503f31391ba26b212fdd86f6a6eff
nafiBUETMe <syedakil.nafi@gmail.com>
2024-06-30 05:12:39 +0600

    Interplanetar Assignment

---
+++ command_control_ws/build/commandControl/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/command_control_ws/src/commandControl
+# Build directory: /home/vboxuser/command_control_ws/build/commandControl
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
