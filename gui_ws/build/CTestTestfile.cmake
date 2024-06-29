073ca2c6c995be3ff55fe94e231ad3fbfc8d1d15
nafiBUETMe <syedakil.nafi@gmail.com>
2024-06-30 05:04:43 +0600

    Added GUI Code

---
+++ gui_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/gui_ws/src
+# Build directory: /home/vboxuser/gui_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("combined_gui")
